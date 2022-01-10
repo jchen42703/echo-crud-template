90f4ee9b96819a9dbbb357285ed317fa004ed8dc
Joseph Chen <jchen42703@gmail.com>
2022-01-10 13:36:49 -0500

    Created MonthFromNow

--- internal/templates/time.go
+++ internal/templates/time.go
@@ -6,6 +6,10 @@ func DayFromNow() time.Time {
 	return time.Now().Add(24 * time.Hour)
 }
 
+func MonthFromNow() time.Time {
+	return time.Now().Add(30 * 24 * time.Hour)
+}
+
 // For redis expiration
 func DayInSeconds() string {
 	return "86400"
